e307b465dcc767abe0abb1f87b035ea9b6d0fde0
Mounika262 <mounikagottipalli24@gmail.com>
2024-08-28 22:44:32 +0530

    Update Courses.jsx

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -13,7 +13,7 @@ const Courses = () => {
 
   const fetchCourses = async () => {
     try {
-      const response = await axios.get("http://localhost:8085/api/courses/");
+      const response = await axios.get("http://3.107.58.122:8081/api/courses/");
       setCourses(response.data);
     } catch (error) {
       console.error("Error fetching courses:", error);
@@ -24,7 +24,7 @@ const Courses = () => {
   const handleDelete = async (id) => {
     try {
       const response = await axios.delete(
-        `http://localhost:8085/api/courses/${id}`
+        `http://3.107.58.122:8081/api/courses/${id}`
       );
       if (response.status === 200) {
         alert("Course deleted successfully");
